2eabd5dec378feb9132710b1747953a29304c5a0
Derek Young <yderek5@gmail.com>
2022-02-20 22:16:02 -0600

    graphql server and astronauts service working

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const port = 4000;
+
+const gateway = new ApolloGateway({
+  serviceList: [{ name: "astronauts", url: "http://localhost:4001" }]
+})
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false
+});
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
